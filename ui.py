46d1d835d303676b0c29e60fd4454f1cfe1bafe4
Buzato <joaobuzato@gmail.com>
2022-04-27 19:48:43 -0300

    adding quiz UI components and next question

--- ui.py
+++ ui.py
@@ -1,16 +1,31 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 THEME_COLOR = "#375362"
 FONT = ("Arial", 20, "italic")
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.configure(bg=THEME_COLOR)
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
         self.score_label = Label(text="Score: 0", font=FONT, bg=THEME_COLOR, fg="white")
         self.score_label.grid(column=1, row=0)
-        self.question_canvas = Canvas(width=300, height=250)
-        self.question_canvas.create_text(150,125, text="TEXTO TESTE", font=FONT)
-        self.question_canvas.grid(column=0, row=1, columnspan=2, padx=20)
+        self.question_canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.question_canvas.create_text(150,125, width=280, text="TEXTO TESTE", font=FONT)
+        self.question_canvas.grid(column=0, row=1, columnspan=2, pady=50)
+        true_image = PhotoImage(file="images/true.png")
+        false_image = PhotoImage(file="images/false.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
         self.window.mainloop()
+
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.question_canvas.itemconfig(self.question_text, text=q_text)
